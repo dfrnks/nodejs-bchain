dc33a79dc7a7394a5511d921ae83e7024cd39ff5
dfrnks <douglasfrancardoso@gmail.com>
2020-04-04 11:58:24 -0300

    Create a block

---
+++ block.js
@@ -0,0 +1,36 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(timestamp, lastHash, hash, data) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString() {
+        return `Block
+            Timestamp: ${this.timestamp}
+            Last Hash: ${this.lastHash.substr(0, 10)}
+            Hash     : ${this.hash.substr(0, 10)}
+            Data     : ${this.data}`;
+    }
+
+    static genesis() {
+        return new this('Genesis time', '------', 'ZZSdgcr2DRktaMNv', [])
+    }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = this.hash(timestamp, lastHash, data);
+
+        return new this(timestamp, lastHash, hash, data);
+    }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
+}
+
+module.exports = Block;
