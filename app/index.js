3b1978affe459076859145d09d72abdc7e286c03
dfrnks <douglasfrancardoso@gmail.com>
2020-04-13 17:43:28 -0300

    Add Rewards and miners transactions

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
+const Miner = require('./miner');
 const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -12,6 +13,7 @@ const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(bodyParser.json());
 
@@ -38,6 +40,13 @@ app.post('/transact', (req, res) => {
     const transaction = wallet.createTransaction(recipient, amount, tp);
     p2pServer.broadcastTransaction(transaction);
     res.redirect('/transactions');
+});
+
+app.get('/mine-transactions', (req, res) => {
+    const block = miner.mine();
+
+    console.log(`New block added: ${block.toString()}`);
+    res.redirect('/blocks');
 });
 
 app.get('/public-key', (req, res) => {
---
+++ app/miner.js
@@ -0,0 +1,36 @@
+const Wallet = require('../wallet');
+const Transaction = require('../wallet/transaction');
+
+class Miner {
+    constructor (blockchain, transactionPool, wallet, p2pServer) {
+        this.blockchain= blockchain;
+        this.transactionPool = transactionPool;
+        this.wallet = wallet;
+        this.p2pServer = p2pServer;
+    }
+
+    mine () {
+        const validTransactions = this.transactionPool.validTransactions();
+
+        // Include a reward for the miner
+        validTransactions.push(
+            Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet())
+        );
+
+        // Create a block consisting of the valid transactions
+        const block = this.blockchain.addBlock(validTransactions);
+
+        // Synchroniza the chains in the peer-to-peer server
+        this.p2pServer.syncChains();
+
+        // Clear the transaction pool
+        this.transactionPool.clear();
+
+        // Broadcast to every miner to clear their transaction pools
+        this.p2pServer.broadcastClearTransactions();
+
+        return block;
+    }
+}
+
+module.exports = Miner;
